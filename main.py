556fe1f1920ffa660e7ccd6f336fbe2713f3f661
AdamAlbaghali <adamalbaghali@gmail.com>
2024-06-13 14:16:06 -0400

    Update main.py
    final touches

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def print_slot_machine(columns):
 
 def deposit():
     while True:
-        amount = input("what would you like to deposit?$ ")
+        amount = input("What would you like to deposit? $ ")
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
@@ -79,3 +79,64 @@ def deposit():
         else:
             print("Please enter a number")
     return amount
+def get_number_of_lines():
+    while True:
+        lines = input("Enter number of lines that you want to deposit ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1<= lines <=MAX_LINES:
+                break
+            else:
+                print("Enter valid number of lines")
+
+        else:
+            print("Please enter a number")
+    return lines
+
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? $ ")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET<= amount <=MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}")
+
+        else:
+            print("Please enter a number")
+    return amount
+
+def spin(balance):
+    lines = get_number_of_lines()
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+        if total_bet> balance:
+            print(f"You do not have enough to bet that amount, your current balance id: ${balance}")
+        else:
+            break
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+
+    slots = get_slot_machine_spin(ROWS,COLS, symbol_count)
+    print_slot_machine(slots)
+    winnings,winning_lines=check_winnings(slots, lines,bet,symbol_value)
+    print(f"YOU WON ${winnings}")
+    print(f"YOU WON ON LINES: ", *winning_lines)
+    return winnings - total_bet
+
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is ${balance}")
+        answer=input("Press Enter to play the game(q to quit): ")
+        if answer == "q":
+            break
+        balance += spin(balance)
+
+    print(f"You Left with ${balance}")
+    
+
+
+main()
