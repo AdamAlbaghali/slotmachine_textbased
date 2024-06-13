22b08ed16260cf4048ce438eab0bf805a6dcac34
AdamAlbaghali <adamalbaghali@gmail.com>
2024-06-13 14:14:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -54,3 +54,28 @@ def get_slot_machine_spin(rows,cols,symbols):
         columns.append(column)
 
     return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i , column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end =" | ")
+
+            else:
+                print(column[row], end="")
+
+        print()
+
+def deposit():
+    while True:
+        amount = input("what would you like to deposit?$ ")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount should be greater than zero")
+
+        else:
+            print("Please enter a number")
+    return amount
